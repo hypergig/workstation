89c404dcdf8216356004cdfff2c8cda0bfe6222b
Jordan Cohen <hypergig@gmail.com>
2019-09-03 16:36:15 -0400

    gcloud install

---
+++ apps/gcloud.sh
@@ -0,0 +1,6 @@
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+apts+=' google-cloud-sdk kubectl'
+tests['gcloud']="gcloud --version"
+tests['kubectl']="kubectl version --client"
