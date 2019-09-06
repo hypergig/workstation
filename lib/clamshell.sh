d862f7d7e697d19fcafa49775ecdb456e53ff9f0
Jordan Cohen <hypergig@gmail.com>
2019-09-06 16:36:23 -0400

    clamshell bug fix and exec the exec

--- lib/clamshell.sh
+++ lib/clamshell.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
+set -euo pipefail
+
+# check if sway is bran new, if so give it some time to finish booting before
+# yanking displays from under it
+if [ $(ps -o etimes= -p $(pidof sway)) -lt 10 ]; then
+    # TODO - figure out what we are actually waiting for and check that instead
+    # of a random sleep
+    echo "sway is just starting up, lets give her 10 seconds to get her shit together..."
+    sleep 2
+fi
+
 
 state=enable
 if [[ $(swaymsg -t get_outputs) =~ Dell ]]; then
