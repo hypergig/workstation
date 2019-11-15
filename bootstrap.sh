405dc53a26cfa48cf677b9027618a69007a6f4c2
Jordan Cohen <hypergig@gmail.com>
2019-11-15 12:34:01 -0500

    bug fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,8 @@ set -xeuo pipefail
 mkdir -vp ~/repos
 cd ~/repos
 sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y git make ansible
+sudo apt-get install -y git make python3-pip
+pip3 install --user ansible jmespath
 [ -d 'workstation' ] || git clone git@github.com:hypergig/workstation.git
 cd workstation
 exec make install
