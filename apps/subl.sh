d3a6fdb300b5eb03b099e32ba13dd897136e1945
Jordan Cohen <hypergig@gmail.com>
2019-11-16 01:14:34 -0500

    clean up

--- apps/subl.sh
+++
@@ -1,5 +0,0 @@
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-
-apts+=' sublime-text'
-tests['sublime']="subl --version"
