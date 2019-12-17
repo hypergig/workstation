fb099207b88c88cedc059e4f433ef1536ac293a9
Jordan Cohen <hypergig@gmail.com>
2019-12-17 13:13:48 -0500

    screenshot support

---
+++ lib/screenshot.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+set -euo pipefail
+
+mkdir -vp  ~/screenshots
+
+[ "${1:-}" != "full" ] && geo="$(slurp -d)"
+grim ${geo:+-g "${geo}"} ~/screenshots/$(date --iso-8601=sec | sed 's/\(:\|,\)/-/g')
