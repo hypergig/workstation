ce13124dfe97fe9ab9010838458b2ac9ad3b45f3
Jordan Cohen <hypergig@gmail.com>
2020-03-02 10:25:24 -0500

    clamshell notify

--- lib/clamshell.sh
+++ lib/clamshell.sh
@@ -16,5 +16,4 @@ if [[ $(swaymsg -rt get_outputs | jq .[].make) =~ Goldstar|Dell ]]; then
     state=disable
 fi
 swaymsg output eDP-1 ${state}
-# TODO - use the notify daemon.. when you get one
-# swaynag -t warning -m "main display is ${state} - code ?"
+notify-send -i preferences-desktop-display Sway "Laptop screen ${state}"
