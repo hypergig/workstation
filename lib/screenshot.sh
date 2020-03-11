1e522702d96b710364ace3de1064166f36af572b
Jordan Cohen <hypergig@gmail.com>
2020-03-11 12:15:31 -0400

    screenshot bug

--- lib/screenshot.sh
+++ lib/screenshot.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 mkdir -vp  ~/screenshots
 
 [ "${1:-}" != "full" ] && geo="$(slurp -d)"
-grim ${geo:+-g "${geo}"} ~/screenshots/$(date --iso-8601=sec | sed 's/\(:\|,\)/-/g')
+grim ${geo:+-g "${geo}"} ~/screenshots/$(date --iso-8601=sec | sed 's/\(:\|,\)/-/g').png
