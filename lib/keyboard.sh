4036caf7ea2bc828a7438a95479d242375d9e00a
Jordan Cohen <hypergig@gmail.com>
2020-03-16 17:32:44 -0400

    Sway to regolith migration (#2)
    * regolith install
    
    * removing sway - I will come back one day... it isn't your fault.. the world just isn't ready for you yet...... :(
    
    * removing alacritty - regolith comes with it's own term
    
    * removing gtk themes - regolith has a solarized theme (look) already
    
    * house cleaning

--- lib/keyboard.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# keyboard hacks
-
-# make caps lock an additional backspace with repeat
-setxkbmap -option caps:backspace
-setxkbmap -option shift:both_capslock
-xmodmap -e "clear Lock"
--- lib/n_term.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-wmctrl -i -a $(wmctrl -lx | grep "gnome-terminal-server.Gnome-terminal" | sed -n ${@}p) || exec gnome-terminal.wrapper
