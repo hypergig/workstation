58e9e086181be1953199f771f44c2e77ba95f81b
Jordan Cohen <hypergig@gmail.com>
2020-06-22 18:41:43 -0400

    migrating to popos

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ sudo apt-get install -y git make python3-pip
 pip3 install --user ansible jmespath
 [ -d 'workstation' ] || git clone git@github.com:hypergig/workstation.git
 cd workstation
-PATH="~/.local/bin:${PATH}"
+PATH="~/.local/bin:/snap/bin:${PATH}"
 exec make install
